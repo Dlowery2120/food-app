ca45ff44f0bb85994bb7c58b7aee954356a66d94
Devante Lowery <dlowery2120@gmail.com>
2021-04-20 09:38:16 -0500

    added tables

--- db/migrate/20210419222943_create_reviews.rb
+++ db/migrate/20210419222943_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-
+      t.integer :rating
+      t.text :comment
+      t.integer :user_id
+      t.integer :recipe_id
+      
       t.timestamps
     end
   end
